8dcb0e6fae81b3d924ef3fdce27ba700bf107181
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-11 13:52:27 +0200

    update

--- main.js
+++ main.js
@@ -14,4 +14,18 @@ function createWindow() {
   });
 
   mainWindow.loadFile('index.html');
+  // Open window devtools
+  //   mainWindow.webContents.openDevTools();
 }
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
