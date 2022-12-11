565009994439acaca1f9bb84561b865bf11bb398
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-11 12:59:05 +0200

    Create main window

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+      //   enableRemoteModule: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+}
